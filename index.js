ac44bab54ce8ddb2dc71f722527c074ef130e566
Solemar Silva <solemar.silva@tmax.com.br>
2020-09-27 21:13:07 -0300

    instalar express e criar rotas no Node

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
-/* link para documentação: https://nodejs.org/docs/latest-v8.x/api/synopsis.html */
-var http = require('http');
+/* link para documentação Node: https://nodejs.org/docs/latest-v8.x/api/synopsis.html 
+   link para documentação Express https://expressjs.com/pt-br/
+*/
+const express = require("express");
 
-http.createServer(function(req, res){
-    res.end("Gerenciador Financeiro");
-}).listen(8080);
+const app = express();
+
+app.get("/", function(req, res){
+    res.send("Gerenciador Financeiro");
+});
+
+app.get("/sobre-empresa", function(req, res){
+    res.send("Pagina sobre empresa");
+});
+
+app.get("/blog", function(req, res){
+    res.send("Pagina do blog");
+});
+
+app.get("/contato", function(req, res){
+    res.send("Pagina de contato");
+});
+
+//localhost:8080
+app.listen(8080);
